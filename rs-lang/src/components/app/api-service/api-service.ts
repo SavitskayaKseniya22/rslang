c03495482947fd37575a8809c3c53c63295c8e8d
Nikita1814 <kravchenko1814@gmail.com>
2022-02-03 16:24:21 +0300

    add textbook page loading

--- rs-lang/src/components/app/api-service/api-service.ts
+++ rs-lang/src/components/app/api-service/api-service.ts
@@ -1,2 +1,14 @@
-class ApiService {}
+class ApiService {
+  apiUrl: string
+  constructor() {
+    this.apiUrl = `http://127.0.0.1:3000`
+  }
+  async requestWords(grp: number, page: number) {
+    const res = await fetch(`${this.apiUrl}/words?page=${page}&group=${grp}`)
+    if (res.ok){
+        const words = await res.json()
+        return words
+    }
+  }
+}
 export default ApiService
--- rs-lang/src/components/app/core/text-book-page/text-book-page.ts
+++ rs-lang/src/components/app/core/text-book-page/text-book-page.ts
@@ -1,13 +1,18 @@
-import ApiService from "../../api-service/api-service"
-import "./text-book-page.css"
+import ApiService from '../../api-service/api-service'
+import { Word } from '../../interfaces/interfaces'
+import './text-book-page.css'
 
-class TextBookPage{
-    service: ApiService
-    constructor(service: ApiService){
-        this.service = service
-    }
-    render(){
-        document.querySelector('.main').innerHTML = `<div class="textbook-container">
+class TextBookPage {
+  service: ApiService
+  curPage: number
+  curGrp: number
+  constructor(service: ApiService) {
+    this.service = service
+    this.curPage = 0
+    this.curGrp = 0
+  }
+  render() {
+    document.querySelector('.main').innerHTML = `<div class="textbook-container">
         <div class="tb-mini-game-select">
             <div class="tb-minigame"><i class="fas fa-running"></i> sprint</div>
             <div class="tb-minigame"><i class="fas fa-volume-up"></i> audio-challenge</div>
@@ -27,29 +32,44 @@ class TextBookPage{
             <div class="group-select" data-grp="5">6</div>
         </div>
         <div class="tb-words">
-            <div class="tb-word" data-tb-wrd-id="0">
-                <div class="tb-img"></div>
-                <div class="tb-word-info">
-                <div class="tb-word-title-translation-pronounciation">
-                    <h3 class="tb-word-title">place [pleis]</h3>
-                    <h3 class="tb-word-translation"></h3>
-                    <button class="pronounce" data-tb-audio-id="0"><i class="fas fa-volume-up"></i></button>
-                </div>
-                <div class="tb-word-definition">
-                    <p class="tb-definition-english">lorem ipsum</p>
-                    <p class="tb-definition-translation">dolor sit amen</p>
-                </div>
-                <div class="tb-word-sentence">
-                    <p class="tb-sentence-english">lorem ipsum</p>
-                    <p class="tb-sentence-translation">dolor sit amen</p>
-                </div>
-                </div>
-            </div>
+            
         </div>
         
         
     </div>`
-    }
+    this.getWords()
+  }
+  async getWords() {
+    const words = await this.service.requestWords(this.curGrp, this.curPage)
+    words.forEach((word) => {
+      this.drawWord(word)
+    })
+  }
+  drawWord(word: Word) {
+    document.querySelector(`.tb-words`).innerHTML += `
+    <div class="tb-word" data-tb-wrd-id=${word.id}>
+    <img class="tb-img" src=${this.service.apiUrl}/${word.image}>
+    <div class="tb-word-info">
+    <div class="tb-word-title-translation-pronounciation">
+        <h3 class="tb-word-title">${word.word} ${word.transcription}</h3>
+        <h3 class="tb-word-translation">${word.wordTranslate}</h3>
+        <button class="pronounce" data-tb-audio-btn-id=${word.id}><i class="fas fa-volume-up"></i></button>
+    </div>
+    <div class="tb-word-definition">
+        <p class="tb-definition-english">${word.textMeaning}</p>
+        <p class="tb-definition-translation">${word.textMeaningTranslate}</p>
+    </div>
+    <div class="tb-word-sentence">
+        <p class="tb-sentence-english">${word.textExample}</p>
+        <p class="tb-sentence-translation">${word.textExampleTranslate}</p>
+    </div>
+    </div>
+<audtio src=${this.service.apiUrl}/${word.audio} data-tb-audio-id=${word.id} data-tb-audio-num="0"></audtio>
+<audtio src=${this.service.apiUrl}/${word.audioMeaning} data-tb-audio-id=${word.id} data-tb-audio-num="1"></audtio>
+<audtio src=${this.service.apiUrl}/${word.audioExample} data-tb-audio-id=${word.id} data-tb-audio-num="2"></audtio>
+</div>
+    `
+  }
 }
 
 export default TextBookPage
