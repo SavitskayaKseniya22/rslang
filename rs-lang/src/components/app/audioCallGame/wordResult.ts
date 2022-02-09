8aff4c3a4d8691d0f99d3d7a2228191d330c8fa9
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2022-02-09 14:24:38 +0300

    fix: wordResult.ts

--- rs-lang/src/components/app/audioCallGame/wordResult.ts
+++ rs-lang/src/components/app/audioCallGame/wordResult.ts
@@ -2,22 +2,22 @@ import ImageIconVoice from "./imageIconVoice";
 import { Word } from "./type";
 
 class WordResult {
-  wrapperForWord = document.createElement("div");
-  titleWord = document.createElement("h4");
-  buttonSound = document.createElement("button");
-  spanTranslateWord = document.createElement("span");
   addWrapperForWord(arrayTrueWords: Word[], count: number) {
+    const wrapperForWord = document.createElement("div");
+    const titleWord = document.createElement("h4");
+    const buttonSound = document.createElement("button");
+    const spanTranslateWord = document.createElement("span");
     if (arrayTrueWords.length > 0) {
-      this.titleWord.innerHTML = arrayTrueWords[count].word;
-      this.spanTranslateWord.innerHTML = ` - ${arrayTrueWords[count].wordTranslate}`;
-      this.titleWord.append(this.spanTranslateWord);
-      this.buttonSound.classList.add("voice-icon-result");
-      this.buttonSound.setAttribute("id", arrayTrueWords[count].audio)
-      this.buttonSound.prepend(new ImageIconVoice().addIconVoice("images/iconVoiceForResult.svg"));
-      this.wrapperForWord.append(this.buttonSound, this.titleWord);
-      this.wrapperForWord.classList.add("answer-content");
-      return this.wrapperForWord;
-    } else return this.wrapperForWord;
+      titleWord.innerHTML = arrayTrueWords[count].word;
+      spanTranslateWord.innerHTML = ` - ${arrayTrueWords[count].wordTranslate}`;
+      titleWord.append(spanTranslateWord);
+      buttonSound.classList.add("voice-icon-result");
+      buttonSound.setAttribute("id", arrayTrueWords[count].audio)
+      buttonSound.prepend(new ImageIconVoice().addIconVoice("images/iconVoiceForResult.svg"));
+      wrapperForWord.append(buttonSound, titleWord);
+      wrapperForWord.classList.add("answer-content");
+      return wrapperForWord;
+    } else return wrapperForWord;
   }
 }
 export default WordResult;
